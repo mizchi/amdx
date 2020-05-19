e9c1252fb7e040700792cb3522588d7cd35dc515
yamanoku <0910yama@gmail.com>
2020-05-19 22:14:14 +0900

    fix:link-color-contrast

--- amdxg/amdxg-components/src/Link.tsx
+++ amdxg/amdxg-components/src/Link.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 export function Link(props: { href: string; children: React.ReactNode }) {
   return (
-    <a className="text-blue-500 hover:text-blue-800" href={props.href}>
+    <a className="text-blue-700 hover:text-blue-800" href={props.href}>
       {props.children}
     </a>
   );
